aca5fabc9b87b354828857bcb2c61fadfdba517c
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-03-13 10:06:03 -0700

    Complete review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "Matt";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
